3c2f008c2bca43013527f0f29873d6d6160f1543
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-12-04 13:44:07 -0500

    completed day 3

---
+++ day3.py
@@ -0,0 +1,40 @@
+# Problem: https://adventofcode.com/2020/day/1
+# Date: December 2020
+# Author: Alex Nazareth
+
+import numpy as np
+import time
+
+def part1():
+    in_filename = 'day3_in.txt'
+    in_data = np.loadtxt(in_filename, dtype=str, delimiter='\n', comments=None)
+
+    in_width, ymax = len(in_data[0]), len(in_data)
+    xpos, ypos = 0, 0
+    num_trees = 0
+
+    while ypos<ymax-1:
+        xpos = (xpos + 3)%in_width
+        ypos += 1
+        if in_data[ypos][xpos]=='#':
+            num_trees += 1
+    return num_trees
+
+def part2(dx, dy):
+    in_filename = 'day3_in.txt'
+    in_data = np.loadtxt(in_filename, dtype=str, delimiter='\n', comments=None)
+
+    in_width, ymax = len(in_data[0]), len(in_data)
+    xpos, ypos = 0, 0
+    num_trees = 0
+
+    while ypos<ymax-1:
+        xpos = (xpos + dx)%in_width
+        ypos += dy
+        if in_data[ypos][xpos]=='#':
+            num_trees += 1
+    return num_trees
+
+
+if __name__=='__main__':
+    print(part2(1,1)*part2(3,1)*part2(5,1)*part2(7,1)*part2(1,2))
