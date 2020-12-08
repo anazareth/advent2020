70c29a26e130e6028e39898eb66e8aa26b85fed7
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-12-07 22:52:22 -0500

    day 7 part 2 done

--- day7.py
+++ day7.py
@@ -8,36 +8,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-# def part1_v1():
-#     in_filename = r'data\day7_in.txt'
-#     in_data = np.loadtxt(in_filename, dtype=str, delimiter='\n')
- 
-#     bag_idx = {}
-#     bag_idx['no other']=0
-#     for spec in in_data:
-#         bag_colour, inner_spec = spec.split(' bags contain ')
-#         contents_dict = {}
-#         for i in [x.replace('bags','').replace('bag','').strip('.') for x in inner_spec.split(', ')]:
-#             if i.strip()!='no other':
-#                 qty = i.split(' ')[0]
-#                 inner_colour = i.replace(str(qty)+' ','').strip(' ')
-#                 contents_dict[inner_colour] = qty
-#         bag_idx[bag_colour] = contents_dict
-#     target = 'shiny gold'
-#     num_found = 0
-#     for c in bag_idx:
-#         if type(bag_idx[c]) is int:
-#             if c == target:
-#                 num_found +=1
-#         else:
-#             temp = find_colour(target, bag_idx[c], bag_idx)
-#             if temp is not None:
-#                 num_found += temp
-#     print(num_found)
-
-
 def part1():
     in_filename = r'data\day7_in.txt'
+    # in_filename = r'data\test7.txt'
     in_data = np.loadtxt(in_filename, dtype=str, delimiter='\n')
     
     G = nx.DiGraph()
@@ -53,7 +26,8 @@ def part1():
     for b in G.nodes():
         if b!=target and nx.has_path(G,b,target):
             num_bags+=1
-    print(num_bags)
+    print('part 1 answer: ' + str(num_bags))
+    return G
 
     # pos=nx.spring_layout(G) # positions for all nodes
     # nx.draw_networkx_nodes(G,pos,
@@ -64,6 +38,12 @@ def part1():
     # plt.show()
 
 
+def part2(G):
+    root_node = 'shiny gold'
+    bag_sum = sum_bags(G, root_node)
+    print('part 2 answer: ' + str(bag_sum))
+
+
 def parse_colour(bag_colour, inner_colour):
     x = re.search(r'(^[1-9]*) ([a-z]* [a-z]*).*',inner_colour)
     if x:
@@ -72,20 +52,20 @@ def parse_colour(bag_colour, inner_colour):
         return (bag_colour, colour, qty)
     return None
 
-
-
-
-# def find_colour(colour, spec_dict, bag_list):
-#     if colour in spec_dict:
-#         return 1
-#     else:
-#         for c in spec_dict:
-#             return find_colour(colour, bag_list[c], bag_list)
-
-def part2():
-    print(-2)
+def sum_bags(G, bag_colour):
+    child_bags = G.adj[bag_colour]
+    if len(child_bags)==0:
+        return 0
+    else:
+        temp = 0
+        for c, w in dict(child_bags).items():
+            temp += int(w['weight']) + int(w['weight'])*int(sum_bags(G, c))
+            print(c,w)
+        print('--')
+        print(str(w['weight']) + ' + ' + str(w['weight']) + '*' + str(temp))
+        return temp
 
 
 if __name__=='__main__':
-    part1()
-    part2()
+    G = part1()
+    part2(G)
