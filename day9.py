3fcbc794645d98caf3755fd649d0187fcacd4127
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-12-09 14:15:27 -0500

    running sum for efficiency

--- day9.py
+++ day9.py
@@ -21,20 +21,23 @@ def check_sum(in_data, sum_pos, lag_size):
                 return True
     return False
 
-    
-def part2(in_data, bad_number):
+
+def part2(in_data, bad_number):  # 23761694.0
     n = len(in_data)
     for i in range(n):
+        sums = in_data[i]
         for j in range(i+1, n):
-            contiguous_range = in_data[i:j]
-            if sum(contiguous_range) == bad_number:
-                return min(contiguous_range) + max(contiguous_range)
+            if sums == bad_number:
+                print(i,j)
+                return min(in_data[i:j]) + max(in_data[i:j])
+            sums += in_data[j]
     return -2
 
 
 if __name__=='__main__':
     lag_size = 25
     in_filename = r'data\day9_in.txt'
+    # in_filename = r'data\test9.txt'
     in_data = np.loadtxt(in_filename, dtype=float, delimiter='\n')
     bad_number = part1(in_data, lag_size)
     print('Part 1 solution: ' + str(bad_number))
