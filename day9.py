f9dea0e9720eeb63ef5ecc0646ccd0a4d3e159f5
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-12-09 13:56:52 -0500

    this one was easy

---
+++ day9.py
@@ -0,0 +1,41 @@
+# Problem: https://adventofcode.com/2020/day/9
+# Date: December 2020
+# Author: Alex Nazareth
+
+import numpy as np
+import re
+
+
+def part1(in_data, lag_size):
+    n = len(in_data)
+    for sum_pos in range(lag_size,n):
+        if check_sum(in_data, sum_pos, lag_size) is False:
+            return in_data[sum_pos]
+    return -1
+
+
+def check_sum(in_data, sum_pos, lag_size):
+    for i in range(sum_pos-lag_size, sum_pos-1):
+        for j in range(i+1, sum_pos):
+            if in_data[i] + in_data[j] == in_data[sum_pos]:
+                return True
+    return False
+
+    
+def part2(in_data, bad_number):
+    n = len(in_data)
+    for i in range(n):
+        for j in range(i+1, n):
+            contiguous_range = in_data[i:j]
+            if sum(contiguous_range) == bad_number:
+                return min(contiguous_range) + max(contiguous_range)
+    return -2
+
+
+if __name__=='__main__':
+    lag_size = 25
+    in_filename = r'data\day9_in.txt'
+    in_data = np.loadtxt(in_filename, dtype=float, delimiter='\n')
+    bad_number = part1(in_data, lag_size)
+    print('Part 1 solution: ' + str(bad_number))
+    print('Part 2 solution: ' + str(part2(in_data, bad_number)))
