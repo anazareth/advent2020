045cd8cc8754436610d33062aac58854de4b0ac5
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-12-11 22:09:35 -0500

    cleanup, yes it is slow

--- day11.py
+++ day11.py
@@ -12,7 +12,6 @@ def part1(in_data):
     prev_seating_matrix = None
     while True:
         seating_matrix, num_occupied = update_seating(seating_matrix)
-        print(seating_matrix)
         if np.array_equal(prev_seating_matrix, seating_matrix):
             return num_occupied
         else:
@@ -40,8 +39,6 @@ def sum_adjacent(seating_matrix, i, j):
     sum_adj_occ = 0
     i_max = len(seating_matrix)
     j_max = len(seating_matrix[0])
-    if i==0 and j==8:
-        print('whats going on here')
     for x in range(max(i-1, 0), min(i+2, i_max)):
         for y in range(max(j-1, 0), min(j+2, j_max)):
             if seating_matrix[x][y] == '#' and (i!=x or j!=y):
@@ -58,7 +55,5 @@ if __name__=='__main__':
     # in_filename = r'data\test11.txt'
     in_data = np.loadtxt(in_filename, dtype=str)
     in_data2 = np.array([list(r) for r in in_data])
-    print(len(in_data2))
-    print(len(in_data2[0]))
     print('Part 1 solution: ' + str(part1(in_data2)))
     # print('Part 2 solution: ' + str(part2(in_data)))
