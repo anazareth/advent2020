ae051e2bc0d129fa39bb9d9ad2964b1bba237fee
anazareth <anazareth@ryerson.ca>
2020-12-14 23:30:54 -0500

    add some comments, clean up

--- day14.py
+++ day14.py
@@ -42,9 +42,9 @@ def part2(rows):
                 elif b == 'X':
                     floatings_mask.append(i)
         else:
-            addr = int(lhs[4:-1])
-            val = int(rhs)
+            addr, val = int(lhs[4:-1]), int(rhs)
             addr_bin = '{:036b}'.format(int(addr))
+            # create masked memory address, and then handle "floating" bits
             updated_addr_bin = addr_bin
             for i in ones_mask:
                 updated_addr_bin = updated_addr_bin[:i] + '1' + updated_addr_bin[i+1:]
@@ -58,20 +58,20 @@ def part2(rows):
 
 
 def get_mem_locs(binary_addr, floats, mem_locs):
+    # recursive function to get all permutations of memory locations due to "floating" positions
     float_loc = floats[0]
     updated_binary_addr = [0, 0]
-    for i in [0, 1]:
+    for i in [0, 1]:  # two "child" cases - subbing 0 or 1
         updated_binary_addr[i] = binary_addr[:float_loc] + str(i) + binary_addr[float_loc+1:]
-        if 'X' not in updated_binary_addr[i]:
+        if 'X' not in updated_binary_addr[i]:  # base case
             mem_locs = mem_locs.union([updated_binary_addr[i]])
         else:
             mem_locs = mem_locs.union(get_mem_locs(updated_binary_addr[i], floats[1:], mem_locs))
     return mem_locs
 
 
-
 if __name__ == '__main__':
-    # in_filename = r'data\test14b.txt'
     in_filename = r'data\day14_in.txt'
     in_data = np.loadtxt(in_filename, dtype=str, delimiter='\n')
-    print(part2(in_data))
+    print('Answer for part 1: ' + str(part1(in_data)))
+    print('Answer for part 2: ' + str(part2(in_data)))
